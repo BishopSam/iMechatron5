672fd3601803c36065928fe663c29442f4cd4f56
Amusuo Paschal <amusuopaschal@gmail.com>
2020-01-13 14:08:50 +0100

    Added Activities for calculating CGPA and displaying result

--- app/src/main/java/com/example/imechatron/GpCalculateActivity.java
+++ app/src/main/java/com/example/imechatron/GpCalculateActivity.java
@@ -1,80 +1,240 @@
 package com.example.imechatron;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
+import static com.example.imechatron.ResultActivity.KEY_CGPA;
+import static com.example.imechatron.ResultActivity.KEY_COURSES;
 
 public class GpCalculateActivity extends AppCompatActivity {
-    Button btGPA;
 
-    Spinner tnu, grade1, creditunit1, grade2, creditunit2, grade3, creditunit3, grade4, creditunit4, grade5 , creditunit5,
-            grade6, creditunit6,
-            grade7, creditunit7,grade8, creditunit8, grade9, creditunit9, grade10, creditunit10, grade11, creditunit11;
+    private LinearLayout parentLayout;
+    private TextView tvAddCourse;
+    private Button btCalculate;
 
-    EditText etSemester, etCode1, etCode2, etCode3, etCode4, etCode5, etCode6, etCode7, etCode8, etCode9, etCode10, etCode11;
+    private ArrayList<LinearLayout> linearLayouts;
+    private ArrayList<EditText> editTexts;
+    private ArrayList<Spinner> unitsSpinners;
+    private ArrayList<Spinner> gradeSpinners;
+    private ArrayList<ImageView> cancelButtons;
+
+    private ArrayList<CourseResult> results;
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gp_calc);
 
-        btGPA=findViewById(R.id.bt_calc);
-        tnu =findViewById(R.id.spinner_tnu);
-        grade1 =findViewById(R.id.spinner1);
-        creditunit1=findViewById(R.id.spinner2);
-
-        grade2 =findViewById(R.id.spinner3);
-        creditunit2=findViewById(R.id.spinner4);
-
-        grade3 =findViewById(R.id.spinner5);
-        creditunit3=findViewById(R.id.spinner6);
-
-        grade4 =findViewById(R.id.spinner7);
-        creditunit4=findViewById(R.id.spinner8);
-
-        grade5 =findViewById(R.id.spinner9);
-        creditunit5=findViewById(R.id.spinner10);
-
-        grade6 =findViewById(R.id.spinner11);
-        creditunit6=findViewById(R.id.spinner12);
-
-        grade7 =findViewById(R.id.spinner13);
-        creditunit7=findViewById(R.id.spinner14);
-
-        grade8 =findViewById(R.id.spinner15);
-        creditunit8=findViewById(R.id.spinner16);
-
-        grade9 =findViewById(R.id.spinner17);
-        creditunit9 =findViewById(R.id.spinner18);
-
-        grade10 =findViewById(R.id.spinner19);
-        creditunit10 =findViewById(R.id.spinner20);
-
-        grade11 =findViewById(R.id.spinner21);
-        creditunit11 =findViewById(R.id.spinner22);
-
-        etSemester=findViewById(R.id.et_semester);
-        etCode1=findViewById(R.id.et1);
-        etCode2=findViewById(R.id.et2);
-        etCode3=findViewById(R.id.et3);
-        etCode4=findViewById(R.id.et4);
-        etCode5=findViewById(R.id.et5);
-        etCode6=findViewById(R.id.et6);
-        etCode7=findViewById(R.id.et7);
-        etCode8=findViewById(R.id.et8);
-        etCode9=findViewById(R.id.et9);
-        etCode10=findViewById(R.id.et10);
-        etCode11=findViewById(R.id.et11);
+        parentLayout = findViewById(R.id.linear_layout);
+        linearLayouts = new ArrayList<>();
+        tvAddCourse = findViewById(R.id.tv_add_course);
+        btCalculate = findViewById(R.id.bt_calculate);
+        cancelButtons = new ArrayList<>();
 
 
 
+        editTexts = new ArrayList<>();
+        unitsSpinners = new ArrayList<>();
+        gradeSpinners = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++){
+            addNewCourse();
+        }
+
+        tvAddCourse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addNewCourse();
+            }
+        });
+
+        btCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String gpa =calculateCGPA();
+                if (gpa == null){
+                    Toast.makeText(GpCalculateActivity.this, "You've not entered any course", Toast.LENGTH_SHORT).show();
+
+
+                } else {
+                    openResultActivity(gpa);
+                }
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        results = new ArrayList<>();
 
     }
 
+    private void openResultActivity(String gpa) {
+        Intent intent = new Intent(this, ResultActivity.class);
+        intent.putExtra(KEY_CGPA, gpa);
+        intent.putExtra(KEY_COURSES, results);
+        startActivity(intent);
+    }
+
+    private void addNewCourse() {
+        LinearLayout linearLayout = new LinearLayout(this);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        layoutParams.setMargins(0, 0, 0,10);
+        linearLayout.setLayoutParams(layoutParams);
 
 
+        EditText editText = new EditText(this);
+        LinearLayout.LayoutParams etLayoutParams = new LinearLayout.LayoutParams(
+                0, LinearLayout.LayoutParams.WRAP_CONTENT, 0.4f);
+        etLayoutParams.setMargins(0, 0, 20,0);
+        editText.setLayoutParams(etLayoutParams);
+        editText.setHint("CSC 201");
+        editText.setBackgroundColor(Color.WHITE);
 
+        Spinner unitSpinner = new Spinner(this);
+        ArrayAdapter<CharSequence> unitAdapter = ArrayAdapter.createFromResource(this,
+                R.array.course_units, android.R.layout.simple_spinner_item);
+        unitAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        unitSpinner.setAdapter(unitAdapter);
+        LinearLayout.LayoutParams unitSpLayoutParams = new LinearLayout.LayoutParams(
+                0, LinearLayout.LayoutParams.WRAP_CONTENT, 0.25f);
+        unitSpLayoutParams.setMargins(0, 0, 20,0);
+        unitSpinner.setLayoutParams(unitSpLayoutParams);
+
+        Spinner gradeSpinner = new Spinner(this);
+        ArrayAdapter<CharSequence> gradeAdapter = ArrayAdapter.createFromResource(this,
+                R.array.course_grade, android.R.layout.simple_spinner_item);
+        gradeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        gradeSpinner.setAdapter(gradeAdapter);
+        LinearLayout.LayoutParams gradeSpLayoutParams = new LinearLayout.LayoutParams(
+                0, LinearLayout.LayoutParams.WRAP_CONTENT, 0.25f);
+        gradeSpLayoutParams.setMargins(0, 0, 20,0);
+        gradeSpinner.setLayoutParams(gradeSpLayoutParams);
+
+        ImageView cancelImage = new ImageView(this);
+        LinearLayout.LayoutParams imageLayoutParams = new LinearLayout.LayoutParams(
+                0, LinearLayout.LayoutParams.WRAP_CONTENT, 0.1f);
+        cancelImage.setLayoutParams(imageLayoutParams);
+        cancelImage.setImageResource(R.drawable.ic_cancel_black_24dp);
+        cancelImage.setId(cancelButtons.size());
+        cancelImage.setId(cancelButtons.size());
+        cancelImage.setOnClickListener(removeCourseListener);
+
+
+        editTexts.add(editText);
+        unitsSpinners.add(unitSpinner);
+        gradeSpinners.add(gradeSpinner);
+        cancelButtons.add(cancelImage);
+        linearLayouts.add(linearLayout);
+
+        linearLayout.addView(editText);
+        linearLayout.addView(unitSpinner);
+        linearLayout.addView(gradeSpinner);
+        linearLayout.addView(cancelImage);
+        parentLayout.addView(linearLayout);
+    }
+
+    private String calculateCGPA() {
+        int numberUnits = 0;
+        double totalGP = 0;
+        double gpa;
+
+        for (int i = 0; i < editTexts.size(); i++){
+            String courseCode = editTexts.get(i).getText().toString().trim();
+            if (courseCode.equals("")){
+                continue;
+            }
+
+            String unitString = unitsSpinners.get(i).getSelectedItem().toString();
+            String gradeString = gradeSpinners.get(i).getSelectedItem().toString();
+            int unit = Integer.parseInt(unitString);
+            int grade = getGradePoint(gradeString);
+            numberUnits += unit;
+            int gradePoint = unit * grade;
+            totalGP += gradePoint;
+            CourseResult result = new CourseResult(courseCode, gradeString, unit);
+            results.add(result);
+        }
+
+        if (results.size() == 0){
+            return null;
+        }
+        gpa = totalGP / numberUnits;
+
+        return String.format("%.2f", gpa);
+
+    }
+
+    private int getGradePoint(String gradeString) {
+        int grade = 0;
+        switch (gradeString){
+            case "A":
+                grade = 5;
+                break;
+
+            case "B":
+                grade = 4;
+                break;
+
+            case "C":
+                grade = 3;
+                break;
+
+            case "D":
+                grade = 2;
+                break;
+
+            case "E":
+                grade = 1;
+                break;
+
+            case "F":
+                grade = 0;
+                break;
+
+        }
+
+        return grade;
+    }
+
+    View.OnClickListener removeCourseListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            int id = view.getId();
+            parentLayout.removeView(linearLayouts.get(id));
+            linearLayouts.remove(id);
+            editTexts.remove(id);
+            unitsSpinners.remove(id);
+            gradeSpinners.remove(id);
+            cancelButtons.remove(id);
+            resetCancelButtonsId();
+        }
+    };
+
+    private void resetCancelButtonsId(){
+        for (int i = 0; i < cancelButtons.size(); i++){
+            cancelButtons.get(i).setId(i);
+        }
+    }
 }
---
+++ app/src/main/java/com/example/imechatron/ResultActivity.java
@@ -0,0 +1,103 @@
+package com.example.imechatron;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class ResultActivity extends AppCompatActivity {
+
+    public static final String KEY_CGPA = "cgpaString";
+    public static final String KEY_COURSES = "courses";
+    private ImageView imageResult;
+    private TextView tvCgpa, tvRemark, tvClass, tvViewCourses;
+
+    ArrayList<CourseResult> courseResults;
+    String cgpaString;
+    DegreeClass degreeClass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        imageResult = findViewById(R.id.image_result);
+        tvCgpa = findViewById(R.id.tv_cgpa);
+        tvRemark = findViewById(R.id.tv_remark);
+        tvClass = findViewById(R.id.tv_class);
+        tvViewCourses = findViewById(R.id.tv_view_courses);
+
+        Intent intent = getIntent();
+        if (intent != null){
+            cgpaString = intent.getStringExtra(KEY_CGPA);
+            courseResults = (ArrayList<CourseResult>)intent.getSerializableExtra(KEY_COURSES);
+            degreeClass = getDegreeClass(cgpaString);
+            populateResult();
+        }
+
+    }
+
+    private DegreeClass getDegreeClass(String cgpaString) {
+        double cgpa = Double.parseDouble(cgpaString);
+        DegreeClass degreeClass;
+        if (cgpa >= 4.50 && cgpa <= 5.00){
+            degreeClass = DegreeClass.FIRST_CLASS;
+        } else if(cgpa >= 3.50 && cgpa <= 4.49){
+            degreeClass = DegreeClass.SECOND_CLASS_UPPER;
+        } else if(cgpa >= 2.50 && cgpa <= 3.49){
+            degreeClass = DegreeClass.SECOND_CLASS_LOWER;
+        } else if(cgpa >= 1.50 && cgpa <= 2.49){
+            degreeClass = DegreeClass.THIRD_CLASS;
+        } else if(cgpa >= 1.00 && cgpa <= 1.49){
+            degreeClass = DegreeClass.PASS;
+        } else {
+            degreeClass = DegreeClass.FAIL;
+        }
+        return degreeClass;
+    }
+
+    private void populateResult() {
+        imageResult.setImageResource(degreeClass.getClassIcon());
+        tvCgpa.setText(cgpaString);
+        tvRemark.setText(degreeClass.getClassRemark());
+        tvClass.setText("You are in " + degreeClass.getClassName());
+        tvViewCourses.setText("View " + courseResults.size() + " Results");
+
+    }
+
+    enum DegreeClass {
+        FIRST_CLASS("First Class", "Excellent", R.drawable.ic_check_circle_black_24dp),
+        SECOND_CLASS_UPPER("Second Class Upper", "Good", R.drawable.ic_check_circle_black_24dp),
+        SECOND_CLASS_LOWER("Second Class Class Lower", "Fair", R.drawable.ic_error_grey_24dp),
+        THIRD_CLASS("Third Class", "You need to buckle up", R.drawable.ic_error_grey_24dp),
+        PASS("Pass", "You no go graduate o", R.drawable.ic_error_red_24dp),
+        FAIL("Fail", "Guy, school no be for you abeg", R.drawable.ic_error_red_24dp);
+
+        String className;
+        String classRemark;
+        int classIcon;
+
+        DegreeClass(String className, String classRemark, int classIcon) {
+            this.className = className;
+            this.classRemark = classRemark;
+            this.classIcon = classIcon;
+
+        }
+
+        public String getClassName() {
+            return className;
+        }
+
+        public String getClassRemark() {
+            return classRemark;
+        }
+
+        public int getClassIcon() {
+            return classIcon;
+        }
+    }
+}
